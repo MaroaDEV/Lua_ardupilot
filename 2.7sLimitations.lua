f08d8636482d264630c42b4add6a9a9d50c5add7
MaoraDEV <dev@aerialmetric.com>
2024-09-06 09:45:01 +0200

    transition also enters state_vtol

--- 2.7sLimitations.lua
+++ 2.7sLimitations.lua
@@ -1,11 +1,18 @@
 -- Ce script permet une limitation dynamique des niveaux de gaz
 
+-- Définir les canaux des servos pour les moteurs 5, 6, 7 et 8, l'argument doit être la fonction du servo et non son channel
+local MOTOR5_FUN = 5
+local MOTOR6_FUN = 6  
+local MOTOR7_FUN = 7  
+local MOTOR8_FUN = 8  
+
+local PWM_sum = 0
 local delay_1min = 1*60*1000 
 local looper = 10
 local bat_v = 50
 local pwm_max = 2000
 local Q_M_PWM_MAX = Parameter()
-Q_M_PWM_MAX:init('Q_M_PWM_MAX')       --get the physical location in parameters memory area so no future name search is done
+Q_M_PWM_MAX:init('Q_M_PWM_MAX')      
 
 
 function state_init()
@@ -22,8 +29,9 @@ function state_safe()
    last_state = 0
    bat_v = battery:voltage(0)
    pwm_max = 2000
-   if quadplane:in_vtol_mode() then 
-        return state_vtol, delay_1min
+   pwm_sum = SRV_Channels:get_output_pwm(MOTOR5_FUN) + SRV_Channels:get_output_pwm(MOTOR6_FUN) + SRV_Channels:get_output_pwm(MOTOR7_FUN) + SRV_Channels:get_output_pwm(MOTOR8_FUN)
+   if quadplane:in_vtol_mode() or pwm_sum > 4100 then 
+        return state_vtol, delay_1min -- on ne veut pas changer la limitation pendant que l'utilisation des moteurs
    end
    if bat_v > 50.4 then 
         pwm_max = 1000 + (50.4/bat_v) * 1000
